222a9dee32f6de9479ed12afbb04b77b5739f8d3
Baltasar García Perez-Schofield <baltasarq@gmail.com>
2017-12-01 15:55:54 +0100

    Delete build.sh

--- build/cordova/build.sh
+++
@@ -1,1 +0,0 @@
-cordova build android --release -- --keystore="../../../../../../../Dropbox/Personal/firmas/devbaltasarq-key.ks" --password=topotamadre74_ --storePassword=topotamadre74_ --alias=baltasarq
